8feaca71332573eea4abd73b5e0076501ce261c8
SebasG <gamboasebas@gmail.com>
2020-05-07 23:28:38 -0600

    Ejemplos Comandos SQL

--- DataBaseApplication/src/databaseapplication/DataBaseApplication.java
+++ DataBaseApplication/src/databaseapplication/DataBaseApplication.java
@@ -7,7 +7,9 @@ package databaseapplication;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -21,17 +23,64 @@ public class DataBaseApplication {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String url = "jdbc:sqlserver://localhost:1433;databasename=Venta de Partes";
-        String user = "nacho1";
-        String password = "1234";
+        String url = "jdbc:sqlserver://localhost:1433;databasename=VentaAutos";
+        String user = "SA";
+        String password = "<B4b0rsh162715>";
         
         try {
+            
             Connection connection = DriverManager.getConnection(url, user, password);
             System.out.println("Connected to Microsoft SQL Server");
+            
+            Statement sqlStatement = connection.createStatement();
+ 
+            ResultSet rs = null;
+
+            //Lista de comandos
+            
+            String queryString = "";/*
+            queryString+="SELECT Modelo FROM Automóvil;";
+            
+            
+            System.out.println("\nQuery string:");
+            System.out.println(queryString);
+
+            //ejecuta los comandos
+            rs=sqlStatement.executeQuery(queryString);
+
+
+            while (rs.next()) {
+                String lastName = rs.getString("Modelo");
+                System.out.println(lastName + "\n");
+            }
+
+            rs.close();
+            
+            rs = null;*/
+            
+            queryString = "";
+            queryString+="INSERT INTO Cliente (Estado) VALUES ('2');";
+            queryString+="INSERT INTO Cliente (Estado) VALUES ('Activo');";
+            queryString+="INSERT INTO Cliente (Estado) VALUES ('3');";
+            
+            queryString+="DELETE FROM Cliente WHERE Estado = '2';";
+            
+            System.out.println("\nQuery string:");
+            System.out.println(queryString);
+            
+            sqlStatement.execute(queryString);
+
+            System.out.println("Closing database connection");
+
+            
+            connection.close();
+            
+            
         } catch (SQLException ex) {
             Logger.getLogger(DataBaseApplication.class.getName()).log(Level.SEVERE, null, ex);
             System.out.println("An error has ocurred");
         }
+          
         
         
     }
